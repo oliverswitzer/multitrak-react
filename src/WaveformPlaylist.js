3a15a4028b29d03f418025bbfbb9cd230ac65b07
Oliver Switzer <oliverswitzer@gmail.com>
2018-05-17 07:50:48 -0400

    .wav files are now downloaded from firebase directly
    NOTES:
    - file downloads are pretty slow, may need to consider async or only using mp3s
    - .env file is just a template for .env.local file which contains actual keys (not tracked)

--- src/WaveformPlaylist.js
+++ src/WaveformPlaylist.js
@@ -1,135 +1,49 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
 import $ from "jquery"
 import * as ExternalWaveformPlaylist from 'waveform-playlist'
+import firebase from 'firebase'
+
+firebase.initializeApp({
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: `${process.env.REACT_APP_PROJECT_ID}.firebaseapp.com`,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    storageBucket: process.env.REACT_APP_BUCKET,
+})
+const storage = firebase.storage()
+const storageRef = storage.ref()
 
 export default class WaveformPlaylist extends Component {
     componentDidMount() {
-            ExternalWaveformPlaylist.init(
-                {
-                    samplesPerPixel: 3000,
-                    mono: true,
-                    waveHeight: 70,
-                    container: document.getElementById('playlist'),
-                    state: 'cursor',
-                    colors: {
-                        waveOutlineColor: '#E0EFF1',
-                        timeColor: 'grey',
-                        fadeColor: 'black'
-                    },
-                    controls: {
-                        show: true,
-                        width: 400
-                    },
-                    zoomLevels: [500, 1000, 3000, 5000]
-                }
-            );
+        const myStemNames = [
+            'Audio11_1.wav',
+            'Audio12_1.wav',
+            'Audio13_1.wav',
+            'BrooklynKit_1.wav',
+            'Brooklyn_1.wav',
+            'Chorusaurus_1.wav',
+            'EndlessSummer.wav',
+            'EndlessSummer_1.wav',
+            'EndlessSummer_3.wav',
+            'EndlessSummer_4.wav',
+            'EndlessSummer_5.wav',
+            'VintageSynthStrings.wav',
+            'VintageSynthStrings_2.wav',
+            'VintageSynthStrings_3.wav'
+        ]
 
-            const playlist = ExternalWaveformPlaylist.init({
-                samplesPerPixel: 3000,
-                mono: true,
-                waveHeight: 70,
-                container: document.getElementById('playlist'),
-                state: 'cursor',
-                colors: {
-                    waveOutlineColor: '#E0EFF1',
-                    timeColor: 'grey',
-                    fadeColor: 'black'
-                },
-                controls: {
-                    show: true,
-                    width: 400
-                },
-                zoomLevels: [500, 1000, 3000, 5000]
+        const getDownloadUrlPromises = myStemNames.map(stemName => storageRef.child(stemName).getDownloadURL())
+
+        Promise.all(getDownloadUrlPromises).then(resolvedUrls => {
+            const stemMapping = myStemNames.map((stemName, i) => {
+                return {name: stemName, src: resolvedUrls[i]}
             })
 
-            playlist.load([
-                {
-                    src: 'assets/media/vermont-stems/Audio11_1.wav',
-                    name: 'Audio11_1',
-                    gain: 0.5,
-                },
-                {
-                    src: 'assets/media/vermont-stems/Audio12_1.wav',
-                    name: 'Audio12_1',
-                    gain: 0.5,
-                },
-                {
-                    src: 'assets/media/vermont-stems/Audio13_1.wav',
-                    name: 'Audio13_1',
-                    gain: 0.5,
-                },
-                {
-                    src: 'assets/media/vermont-stems/BrooklynKit_1.wav',
-                    name: 'BrooklynKit_1',
-                    gain: 0.5,
-                },
-                {
-                    src: 'assets/media/vermont-stems/Brooklyn_1.wav',
-                    name: 'Brooklyn_1',
-                    gain: 0.5,
-                },
-                {
-                    src: 'assets/media/vermont-stems/Chorusaurus_1.wav',
-                    name: 'Chorusaurus_1',
-                    gain: 0.5,
-                },
-                {
-                    src: 'assets/media/vermont-stems/EndlessSummer.wav',
-                    name: 'EndlessSummer',
-                    gain: 0.5,
-                },
-                {
-                    src: 'assets/media/vermont-stems/EndlessSummer_1.wav',
-                    name: 'EndlessSummer_1',
-                    gain: 0.5,
-                },
-                {
-                    src: 'assets/media/vermont-stems/EndlessSummer_3.wav',
-                    name: 'EndlessSummer_3',
-                    gain: 0.5,
-                },
-                {
-                    src: 'assets/media/vermont-stems/EndlessSummer_4.wav',
-                    name: 'EndlessSummer_4',
-                    gain: 0.5,
-                },
-                {
-                    src: 'assets/media/vermont-stems/EndlessSummer_5.wav',
-                    name: 'EndlessSummer_5',
-                    gain: 0.5,
-                },
-                {
-                    src: 'assets/media/vermont-stems/VintageSynthStrings.wav',
-                    name: 'VintageSynthStrings',
-                    gain: 0.5,
-                },
-                {
-                    src: 'assets/media/vermont-stems/VintageSynthStrings_2.wav',
-                    name: 'VintageSynthStrings_2',
-                    gain: 0.5,
-                },
-                {
-                    src: 'assets/media/vermont-stems/VintageSynthStrings_3.wav',
-                    name: 'VintageSynthStrings_3',
-                    gain: 0.5,
-                }
-            ])
-
-            const ee = playlist.getEventEmitter()
-            const $container = $("body")
-
-            $container.on("click", ".btn-play", () => {
-                // console.log("YOYO")
-                ee.emit("play")
-            })
-            $container.on("click", ".btn-stop", () => {
-                // console.log("YOYO")
-                ee.emit("stop")
-            })
+            renderWaveformPlaylist(stemMapping)
+        })
     }
+
     render() {
         return (<div>
-
             <h1>Your song</h1>
 
             <div id="top-bar" className="playlist-top-bar">
@@ -148,5 +62,37 @@ export default class WaveformPlaylist extends Component {
             <div id="playlist"></div>
         </div>)
     }
+}
+
+function renderWaveformPlaylist(stemUrls) {
+    const playlist = ExternalWaveformPlaylist.init({
+        samplesPerPixel: 3000,
+        mono: true,
+        waveHeight: 70,
+        container: document.getElementById('playlist'),
+        state: 'cursor',
+        colors: {
+            waveOutlineColor: '#E0EFF1',
+            timeColor: 'grey',
+            fadeColor: 'black'
+        },
+        controls: {
+            show: true,
+            width: 400
+        },
+        zoomLevels: [500, 1000, 3000, 5000]
+    })
+
+    playlist.load(stemUrls)
+
+    const ee = playlist.getEventEmitter()
+    const $container = $("body")
+
+    $container.on("click", ".btn-play", () => {
+        ee.emit("play")
+    })
+    $container.on("click", ".btn-stop", () => {
+        ee.emit("stop")
+    })
 }
 
